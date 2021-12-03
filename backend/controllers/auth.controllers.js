a73427360ef750d1122f5f3614a90ae6a0b7e5fc
Oscar <oscargm28@hotmail.com>
2021-12-03 12:30:49 +0100

    finished google impl

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -2,32 +2,79 @@ const { request, response } = require("express");
 const Usuario = require("../models/Usuario");
 const bcrypt = require("bcryptjs");
 const { generarJWT } = require("../helpers/jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 exports.login = async (req = request, res = response) => {
-   const { email, password } = req.body;
+  const { email, password } = req.body;
 
-   const usuario = await Usuario.findOne({ email });
+  const usuario = await Usuario.findOne({ email });
 
-   if (!usuario) {
-      return res.status(404).json({ 
-         ok:false,
-         msg: 'Credenciales incorrectas' });
-   }
-   // verificar password
-   const validPassword = bcrypt.compareSync(password, usuario.password);
+  if (!usuario) {
+    return res.status(404).json({
+      ok: false,
+      msg: "Credenciales incorrectas",
+    });
+  }
+  // verificar password
+  const validPassword = bcrypt.compareSync(password, usuario.password);
 
-   if (!validPassword) {
-      return res.status(400).json({ 
-         ok:false,
-         msg: 'El password no es correcto' 
-      });
-   }
-   
-   const token = await generarJWT(usuario._id);
+  if (!validPassword) {
+    return res.status(400).json({
+      ok: false,
+      msg: "El password no es correcto",
+    });
+  }
 
-   res.json({ 
-      ok:true,
-      token });
-   }; 
+  const token = await generarJWT(usuario._id);
 
+  res.json({
+    ok: true,
+    token,
+  });
+};
 
+exports.googleSignIn = async (req = request, res = response) => {
+  const googleToken = req.body.token;
+
+  
+  try {
+     const { name, email, picture } = await googleVerify(googleToken);
+     
+     // un usuario podria registrarse de forma normal y despues querer acceder por Google.Debo manejar esto
+     const usuarioDB = await Usuario.findOne({ email });
+     let usuario;
+     if (!usuarioDB) {
+         // si el usuario no existe en la base de datos, lo creo
+         usuario = new Usuario({
+               nombre: name,
+               email,
+               password: '@@@', //no se usará
+               img: picture,
+               google: true
+         });
+       } else {
+         // si el usuario existe, actualizo su información
+         usuario = usuarioDB;
+         usuario.google = true;
+         // si le mantengo el campo password va a poder acceder por ambos métodos de autenticación.DEbo dejarlo
+       }
+         // guardar el usuario
+         await usuario.save();
+         // generar el token
+         const token = await generarJWT(usuario.id);
+
+    res.json({
+      ok: true,
+      msg: "Usuario logeado correctamente",
+      name,
+      email,
+      picture,
+      token
+    });
+  } catch (err) {
+    return res.status(401).json({
+      ok: false,
+      msg: "Token no valido",
+    });
+  }
+};
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,5 +1,5 @@
 const { check } = require("express-validator");
-const { login } = require("../controllers/auth.controllers");
+const { login, googleSignIn } = require("../controllers/auth.controllers");
 const { validarCampos } = require("../middlewares/validarCampos");
 
 const router = require("express").Router();
@@ -8,10 +8,19 @@ router.post(
   "/",
   [
     check("email", "Email is required").isEmail(),
-    check("password", "Password is required").exists(),
+    check("password", "Password is required").not().isEmpty(),
     validarCampos,
   ],
   login
+);
+
+router.post(
+  "/google",
+  [
+    check("token", "Token is required").not().isEmpty(),
+  validarCampos
+],
+  googleSignIn
 );
 
 module.exports = router;
